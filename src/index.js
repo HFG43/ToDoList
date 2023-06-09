2d3414de0bf53befb4f98a74518b3ff8b19f7262
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-06-09 14:24:17 -0600

    Add Images

--- src/index.js
+++ src/index.js
@@ -1,20 +1,83 @@
 import _ from 'lodash';
-import printMe from './print.js';
+import TodoItems from './todoItems.js';
+import Trash from './TrashBasket.svg';
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
-  const btn = document.createElement('button');
+class TodoList {
+    constructor(){
+       this.todoList = document.getElementById('todo_list');
+       this.todoDescription = document.getElementById('description');
+       this.clearAllButton = document.getElementById('clear_all');
+       this.todos = JSON.parse(localStorage.getItem('todo')) || [];
+       this.todoDescription.addEventListener('keydown',(event) => {
+        if(event.code === "Enter" || event.code === "NumpadEnter") {
+          this.addItem(event)    
+       }
+   });
+       this.clearAll();
+    }
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
+    addItem = (event) => {
+      const description = this.todoDescription.value;
+      let index = this.todos.length;
+      let completed = false;
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+      const todo = new TodoItems(description, index, completed);
+     
+      this.todos = [...this.todos, todo];
+    
+      localStorage.setItem('todo', JSON.stringify(this.todos));
+      
+      this.displayItems();
+      this.todoDescription.value = '';
+    }
 
-  element.appendChild(btn);
+    displayItems = () => {
+        this.clearList();
+          for(let i = 0; i < this.todos.length; i++){
+          const todo = this.todos[i];
+          const todoItem = document.createElement('li');
+          todoItem.classList.add('todo_item');
 
-   return element;
- }
+          const check = document.createElement('input');
+          check.type = 'checkbox';
+          check.classList.add('checkbox');
+          todoItem.appendChild(check);
+                
+          const todoItemDescription = document.createElement('span');
+          todoItemDescription.classList.add('description');
+          todoItemDescription.textContent = `${todo.description}`;
+          todoItem.appendChild(todoItemDescription);
 
- document.body.appendChild(component());
+          const trashIcon = document.createElement('img');
+          trashIcon.src = Trash;
+          trashIcon.classList.add('trash');
+          todoItem.appendChild(trashIcon);
+          trashIcon.id = this.todos[i].index;
+         
+          this.todoList.appendChild(todoItem);
+      }
+      this.removeSelectItem();
+    }
+    
+    clearList = () => {
+        this.todoList.innerHTML = '';
+    }
+
+    removeSelectItem = () => {
+        const removeItems = document.querySelectorAll('.trash');
+        removeItems.forEach((trash) => {
+            trash.addEventListener('click', this.removeItem);
+        });
+    }
+
+    removeItem = (event) => {
+        const itemPosition = Number(event.target.id);
+        const toRemoveItem = this.todos.filter((task) => task.index !== itemPosition);
+        this.todos = [...toRemoveItem];
+        localStorage.setItem('todo', JSON.stringify(this.todos));
+        this.displayItems();
+      }
+  
+}
+const todoList = new TodoList();
