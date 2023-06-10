f204bc36e64b181bc238e34b0b8ca08a041fb87f
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-06-10 13:52:16 -0600

    Review Merge conflict
    As I had 3 branches on PR at the same time, I generated a conflict with some files

---
+++ src/todoItems.js
@@ -0,0 +1,7 @@
+export default class TodoItems {
+  constructor(description, index, completed) {
+    this.description = description;
+    this.index = index;
+    this.completed = completed;
+  }
+}
