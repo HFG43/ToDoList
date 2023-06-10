5b21ce809fdff636612d6532945cf9fa1571d7f6
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-06-10 10:11:15 -0600

    Array and loop with index added

--- src/todoItems.js
+++
@@ -1,7 +0,0 @@
-export default class TodoItems {
-  constructor(description, index, completed) {
-    this.description = description;
-    this.index = index;
-    this.completed = completed;
-  }
-}
--- webpack.config.js
+++ webpack.config.js
@@ -29,10 +29,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
 };
