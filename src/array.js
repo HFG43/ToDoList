975dd9344da655efc889d91b8650a7de8e652468
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-06-10 02:54:45 -0600

    Set array with Todo List Items

---
+++ src/array.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+      description: 'Study React',
+      completed: false,
+      index: 0,     
+    },
+    {
+      description: 'Have a bike ride',
+      completed: true,
+      index: 1,  
+    },
+    {
+      description: 'Practice JavaScript',
+      completed: false,
+      index: 2,  
+    },
+    {
+      description: 'Run a Marathon',
+      completed: true,
+      index: 3,       
+      },
+    {
+      description: 'Lear data structure',
+      completed: false,
+      index: 4,       
+    },  
+    ];
